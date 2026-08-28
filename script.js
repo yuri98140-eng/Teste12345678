0c94d0477a30e56eb00028db09434fae427bbb84
yuri98140-eng <yuri98140@gmail.com>
2026-08-27 22:59:49 -0300

    script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+function openTab(evt, tabName) {
+  const tabContents = document.getElementsByClassName("tab-content");
+  for (let i = 0; i < tabContents.length; i++) {
+    tabContents[i].classList.remove("active");
+  }
+
+  const tabBtns = document.getElementsByClassName("tab-btn");
+  for (let i = 0; i < tabBtns.length; i++) {
+    tabBtns[i].classList.remove("active");
+  }
+
+  document.getElementById(tabName).classList.add("active");
+  evt.currentTarget.classList.add("active");
+}
